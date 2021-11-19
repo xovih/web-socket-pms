ac1b3c9c746d29b57d2bb538d0e81fb3d016b4de
Sofi Rahmatulloh <05817@divmu.pindad.co.id>
2021-11-19 13:42:39 +0700

    socket 1

---
+++ index.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv"
+import { Server } from "socket.io";
+
+dotenv.config()
+const PORT = process.env.PORT
+
+const whiteList = ['http://192.168.197.234:4201', 'http://192.168.196.196:4203']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Socket Client tidak diijinkan !!"))
+    }
+  }
+}
+
+const io = new Server({
+  cors: corsOptions
+})
+
+io.on("connection", (socket) => {
+  console.log("A user has connected !")
+
+  socket.on("updateDashboard", (data) => {
+    console.log("Data from user socket", data)
+    io.emit("getDashboard", data)
+  })
+
+  socket.on("disconnect", () => {
+    console.log("A user has left")
+  })
+})
+
+io.listen(PORT)
