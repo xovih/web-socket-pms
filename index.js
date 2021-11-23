ea4c5f8957a138273f83c953407985c6fa66038d
Sofi Rahmatulloh <05817@divmu.pindad.co.id>
2021-11-23 14:30:27 +0700

    indexref

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ import { Server } from "socket.io";
 dotenv.config()
 const PORT = process.env.PORT || 4502
 
-const whiteList = ['http://192.168.197.234:4201', 'http://192.168.196.196:4203', 'http://pms-mu.pindad.co.id']
+const whiteList = [
+  'http://192.168.197.234:4201',
+  'http://192.168.196.196:4203',
+  'http://pms-mu.pindad.co.id',
+  'http://bn-pms.divmu.pindad.co.id',
+  'http://pms.divmu.pindad.co.id'
+]
+
 const corsOptions = {
   origin: function (origin, callback) {
     if (whiteList.indexOf(origin) !== -1) {
